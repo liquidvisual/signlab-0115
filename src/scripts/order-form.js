da047e5ffbc9033c7a1c0362a5720f2e04c4242e
Mitch <hello@liquidvisual.com>
2015-07-03 15:51:45 +1000

    Form complete

---
+++ src/scripts/order-form.js
@@ -0,0 +1,24 @@
+/*
+    ORDER-FORM.JS - Last updated: 03.07.15
+*/
+//-----------------------------------------------------------------
+// Variables
+//-----------------------------------------------------------------
+//-----------------------------------------------------------------
+// Controls checkbox dropdowns
+//-----------------------------------------------------------------
+
+// $('.checkbox').click(function() {
+
+// 	var checked = $('input[type=checkbox]', $(this)).prop('checked');
+
+//     if (!checked) {
+//     	$(this).removeClass('has-open-dropdown').next('.form-dropdown').addClass('hide').removeClass('animated fadeIn');
+//     } else {
+//     	$(this).addClass('has-open-dropdown').next('.form-dropdown').removeClass('hide').addClass('animated fadeIn');
+//     }
+// });
+
+//-----------------------------------------------------------------
+//
+//-----------------------------------------------------------------

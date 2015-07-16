092f26e02accb18a1126f793bf3ed42b84b563fc
Mitch <hello@liquidvisual.com>
2015-07-16 14:17:45 +1000

    Date picker update

--- src/scripts/date-picker.js
+++ src/scripts/date-picker.js
@@ -1,5 +1,5 @@
 /*
-    DATE-PICKER.JS - Last updated: 19-11-14
+    DATE-PICKER.JS - Last updated: 16-07-15
 */
 //-----------------------------------------------------------------
 // Variables
@@ -10,9 +10,25 @@
 
 // jQuery UI on SELECT elements
 
-if (!Modernizr.inputtypes.date) {
+// if (!Modernizr.inputtypes.date) {
     $(".ui-datepicker").datepicker({ dateFormat: "dd-mm-yy" });
-}
+// }
+
+// var datePicker = $('.ui-datepicker');
+// var touchDevice = $('.touch').length;
+// var isMobile = $(window).width() <= 640;
+
+// if (touchDevice || isMobile) {
+
+// 	datePicker.one('click focus', function(event){
+// 		event.preventDefault();
+// 		$(this).attr('type', 'date');
+// 		$(this).focus();
+// 	})
+
+// } else {
+// 	$(".ui-datepicker").datepicker({ dateFormat: "dd-mm-yy" });
+// }
 
 //-----------------------------------------------------------------
 //

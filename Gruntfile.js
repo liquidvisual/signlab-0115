f28730f4589a65c1fe509faaec09cb3d6cf44ed9
Mitch <hello@liquidvisual.com>
2015-10-23 16:15:46 +1100

    Github update

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,7 +25,8 @@ module.exports = function (grunt) {
       dist: 'dist',
       assets: 'dist/assets',
       port: '9000',
-      git: 'git@github.com:liquidvisual/signlab-0115.git',
+      // git: 'git@github.com:liquidvisual/signlab-0115.git',
+      git: 'https://github.com/liquidvisual/signlab-0115.git',
       // IMPORTANT: Set a baseurl on line 364
     },
     //-----------------------------------------------------
